b53f4bca3a80d467b72f0aee4935e04f49dc5d28
Alona Harnyk <alyonagarnyk@gmail.com>
2022-09-19 11:42:17 +0300

    19.09

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,24 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { GlobalStyles } from 'utils/GlobalStyle';
-import movies from '../data/movies.json';
+import data from '../data/movies.json';
 import { moviesMapper } from '../utils/mapper';
 import { MoviesGallery } from './MoviesGallery/MoviesGallery';
-import Modal from './Modal/Modal'
 
-export default class App extends Component {
-  state = {
-    movies: moviesMapper(movies),
-    currentImage: null
+export const App = () => {
+  const [movies, setMovies] = useState(() => JSON.parse(localStorage.getItem('movies')) ?? moviesMapper(data));
+
+  useEffect(() => {
+    localStorage.setItem('movies', JSON.stringify(movies));
+  }, [movies]);
+
+  const deleteMovie = movieId => {
+    setMovies(prevMovies => prevMovies.filter(({ id }) => id !== movieId));
   };
 
-  componentDidMount() {
-    const savesMovies = localStorage.getItem('movies')
-    if (savesMovies) {
-      this.setState({movies: JSON.parse(savesMovies)})
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { movies } = this.state;
-    if (prevState.movies !== movies) {
-      localStorage.setItem('movies', JSON.stringify(movies))
-    }
-  }
-
-
-  deleteMovie = (movieId) => {
-    this.setState((prevState) => ({
-      movies: prevState.movies.filter(({id}) => id !== movieId)
-    }))
-  }
-
-  openModal = (data) => {
-    this.setState({currentImage: data})
-  }
-
-  closeModal = () => {
-    this.setState({currentImage: null})
-  }
-
-  render() {
-    const {movies, currentImage} = this.state
-    return (
-      <>
-        <MoviesGallery movies={movies} deleteMovie={this.deleteMovie} openModal={this.openModal} />
-        {currentImage && <Modal currentImage={currentImage} closeModal={this.closeModal} />}
-        <GlobalStyles />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <MoviesGallery movies={movies} deleteMovie={deleteMovie} />
+      <GlobalStyles />
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App  from 'components/App';
+import { App }  from 'components/App';
 import './index.css';
 import { theme } from './utils/theme';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
